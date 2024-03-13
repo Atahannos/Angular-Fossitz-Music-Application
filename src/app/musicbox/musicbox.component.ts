44eb7b1224603d2a0930f8460364c85a5ddf1904
Atahannos <94806229+Atahannos@users.noreply.github.com>
2024-03-14 00:50:37 +0300

    just frontend for now
    bla bla

---
+++ src/app/musicbox/musicbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-musicbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './musicbox.component.html',
+  styleUrl: './musicbox.component.scss'
+})
+export class MusicboxComponent {
+
+}
